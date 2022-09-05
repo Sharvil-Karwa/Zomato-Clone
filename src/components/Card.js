b2db892708a318cb89e55db21026397102575292
Sharvil <karwasharvil@gmail.com>
2022-09-05 18:21:28 +0530

    added menu

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { AiFillStar } from "react-icons/ai";
 
-function Card() {
+function Card({ src, title }) {
   return (
     <div>
-      <p>hello</p>
+      <div className="flex fles-row">
+        <div className="rounded-lg shadow-lg bg-white max-w-sm">
+          <a href="#!">
+            <img className="rounded-t-lg" src={src} alt="" />
+          </a>
+          <div className="p-6">
+            <h5 className="text-gray-900 text-xl font-medium mb-2">{title}</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,46 @@
 import React from "react";
+import FoodCard from "./Card";
 
 function Menu() {
-  return <div>mrwi</div>;
+  return (
+    <div className="justify-center text-center">
+      <h1 className="text-3xl font-sans">Order Food Online</h1>
+      <div className="grid grid-cols-4 gap-8 my-7 mx-8 justify-center">
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+        <FoodCard
+          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
+          title="Domino's Pizza"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
