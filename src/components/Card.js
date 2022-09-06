3051a2e80ec5fd7910b3267ccd8c5dff60c48fd3
Sharvil <karwasharvil@gmail.com>
2022-09-06 17:47:10 +0530

    used an api

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 
-function Card({ src, title }) {
+function Card({ src, title, description }) {
   return (
     <div>
       <div className="flex fles-row">
@@ -9,8 +9,15 @@ function Card({ src, title }) {
           <a href="#!">
             <img className="rounded-t-lg" src={src} alt="" />
           </a>
-          <div className="p-6">
-            <h5 className="text-gray-900 text-xl font-medium mb-2">{title}</h5>
+          <div className="flex items-center justify-between p-4">
+            <div className="text-left">
+              <div className="text-lg truncate w-48">{title} </div>
+              <div className="text-sm w-48 truncate">{description}</div>
+            </div>
+            <div className="flex items-center px-1.5 py-0.5 bg-green-700 rounded-lg text-white">
+              4.5
+              <AiFillStar />
+            </div>
           </div>
         </div>
       </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,43 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FoodCard from "./Card";
+import { faker, Faker } from "@faker-js/faker";
+import axios from "axios";
 
 function Menu() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://foodbukka.herokuapp.com/api/v1/menu")
+      .then((res) => {
+        // console.log(res.data.Result);
+        setMenu(res.data.Result);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="justify-center text-center">
       <h1 className="text-3xl font-sans">Order Food Online</h1>
       <div className="grid grid-cols-4 gap-8 my-7 mx-8 justify-center">
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
-        <FoodCard
-          src="https://b.zmtcdn.com/data/pictures/chains/5/18739655/a3f876979c7b1a123ff8d0548d774cb1_o2_featured_v2.jpg?output-format=webp"
-          title="Domino's Pizza"
-        />
+        {menu.map((item) => (
+          <FoodCard
+            id={item._id}
+            src={faker.image.food(500, 400, true)}
+            title={item.menuname}
+            description={item.description}
+          />
+        ))}
       </div>
     </div>
   );
