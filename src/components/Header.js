aea9f8bd56e0460735c38893cae380e43507f266
Sharvil <karwasharvil@gmail.com>
2022-09-12 20:17:04 +0530

    google oauth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Location from "./Location";
 import SearchComponent from "./Search";
 import Login from "./Login";
 import { Link } from "react-router-dom";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { gapi } from "gapi-script";
 
 function Header() {
   const [open, setOpen] = useState(false);
+
+  // const onSuccess = (res) => {
+  //   console.log("[Login Success] currentUser:", res.profileObj);
+  // };
+
+  // const onSuccess2 = (res) => {
+  //   console.log("Logged out");
+  // };
+
+  // const onFailure = (res) => {
+  //   console.log("[Login failed] res:", res);
+  // };
+
+  // useEffect(() => {
+  //   function start() {
+  //     gapi.auth2.init({
+  //       clientId: clientId,
+  //       plugin_name: "chat",
+  //     });
+  //   }
+  //   gapi.load("client:auth2", start);
+  // });
 
   return (
     <div>
@@ -19,7 +43,7 @@ function Header() {
         <div>
           <Location />
         </div>
-        <Link to="/cart">
+        <Link to="/">
           <div
             className="ml-5 text-lg font-extralight font-sans inline"
             onClick={() => setOpen(true)}
@@ -29,6 +53,29 @@ function Header() {
           <div className="ml-5 text-lg font-extralight font-sans inline">
             Sign up
           </div>
+          {/* <div
+            id="signInButton"
+            className="ml-5 text-lg font-extralight font-sans inline"
+          >
+            <GoogleLogin
+              clientId={clientId}
+              buttonText="Login"
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              cookiePolicy={"single_host_origin"}
+              isSignedIn={true}
+            />
+          </div> */}
+          {/* <div
+            id="signOutButton"
+            className="ml-5 text-lg font-extralight font-sans inline"
+          >
+            <GoogleLogout
+              clientId={clientId}
+              buttonText="Logout"
+              onSuccess={onSuccess}
+            />
+          </div> */}
         </Link>
       </div>
       <div id="login">
